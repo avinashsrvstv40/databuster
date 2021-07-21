07a0bbd2a7638303f96477eacdb0a2a32c5f3be9
avinashsrvstv40 <avinashsrvstv40@gmail.com>
2021-07-22 00:22:37 +0800

    Fibonacci series using map

---
+++ Python Basics/map.py
@@ -0,0 +1,12 @@
+cube = lambda x: x**3
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    lis = [0,1]
+    for i in range(2,n):
+        lis.append(lis[i-2] + lis[i-1])
+    return(lis[0:n])
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
