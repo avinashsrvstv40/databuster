b955e16df30d0760d18ecb64c8850b33e2cbf236
avinashsrvstv40 <avinashsrvstv40@gmail.com>
2021-07-25 22:20:32 +0800

    class variables

---
+++ Python Basics/class_variable.py
@@ -0,0 +1,15 @@
+class student:
+    college = 'XYZ'
+    def __init__(self, name, rollno):
+        self.name = name
+        self.rollno = rollno
+    def display(self):
+        print("Name of the student is ", self.name)
+        print("Roll no of the student is ", self.rollno)
+        print("college name is ", student.college)
+
+s1 = student("Ash", 12123)
+s1.display()
+
+s2 = student("Opera", 3423)
+s2.display()
