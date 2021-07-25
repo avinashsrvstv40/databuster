5f235d6a930ce1fd9eb0ffad80580aa9432fc0e1
avinashsrvstv40 <avinashsrvstv40@gmail.com>
2021-07-25 22:22:26 +0800

    Instance and local variable

--- Python Basics/instance_local_var.py
+++ Python Basics/instance_local_var.py
@@ -1,6 +1,6 @@
 class person:
     def __init__(self, name):
-        self.name = name
+        self.name = name    #Instance variable - self.name & local variable - name
     def display(self):
         print("Hello", self.name)
 
