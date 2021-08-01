0ef3aa072b806ba9e2a9956410b7b2afe6ce6348
avinashsrvstv40 <avinashsrvstv40@gmail.com>
2021-08-01 10:42:48 +0800

    Implementing hash table in python

---
+++ DSA in Python/Hash_Table.py
@@ -0,0 +1,32 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+
+        return h % self.MAX
+
+    def __setitem__(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+
+    def __getitem__(self,key):
+        h = self.get_hash(key)
+        return self.arr[h]
+
+    def __delitem__(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+
+t1 = HashTable()
+print(t1.get_hash("march 6"))
+t1['march 6'] = 12
+print(t1['march 6'])
+del t1['march 6']
+print(t1.arr)
+print(t1['march 6'])
